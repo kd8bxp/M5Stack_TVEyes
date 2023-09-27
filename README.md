0b192cb7182200214f07a20daa908cb73d82255c
LeRoy Miller <kd8bxp@aol.com>
2023-09-27 12:46:09 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# M5Stack TVEyes
+
+Modified code from https://github.com/emilyvelasco/TVeyeball for use with the M5Stack Core, and RCA 13.2 Module (Probably will work with the RCA unit as well).  
+
+TVeyes - The png files, provided needed to be re-encoded for the M5Stack to use, and a 2nd eye was added for display-(and see Notes below) but for the most part this is the code provided by Emily.  
+
+TVeyes2 - Added/Changed how the eyes were display, randomized movements, and randomized blinks.  
+
+TVeyes3 - My attempt to add the eyes looking up and down to the left and up and down to right, (it works, but it's not great)  
+
+Video of it working: https://diode.zone/w/wCgkofnT8VLGwsUzWD3UHc  
+
+## Notes
+
+1) My base code is very much like the code used for my Robot TV display, which means, if you use a M5Go base, as you talk (or loud sounds) - you should see that also displayed on the screen.  
+
+2) This might make a good Halloween display, modified even more to display one large eye in the center of the screen. Just random movements, And scary music playing....  
+
+ 
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request
+
+## Support Me
+
+If you find this or any of my projects useful or enjoyable please support me.  
+Anything I do get goes to buy more parts and make more/better projects.  
+https://www.patreon.com/kd8bxp  
+https://ko-fi.com/lfmiller  
+https://www.paypal.me/KD8BXP  
+
+## Other Projects
+
+https://www.youtube.com/channel/UCP6Vh4hfyJF288MTaRAF36w  
+https://kd8bxp.blogspot.com/  
+
+
+## Credits
+
+Copyright (c) 2023 LeRoy Miller
+
+## License
+
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses>
---
+++ TVeyes3/TVeyes3.ino
@@ -0,0 +1,235 @@
+//#include "src/M5Stack/M5Stack.h"
+#include "src/M5GFX/M5UnitRCA.h"
+#include "math.h"
+//#include "eyecenter.jpg.c"
+#include "eyes.c"
+#include "eyes2.c"
+
+//Thank you Emily Velasco https://github.com/emilyvelasco/TVeyeball for providing the idea and png files for this.
+
+//https://javl.github.io/image2cpp/
+//background color: black
+//dithering: binary
+//scaling: original size
+//output: Arduino code (but have to change const uint16_t to const unsigned char, also add pgmspace.h and move PROGMEM)
+//output: Horizontal - 2 byter per pixel (565)
+//output: swap bits in byte: checked
+
+#define M5STACKFIRE_MICROPHONE_PIN 34 //0
+#define HORIZONTAL 216
+
+M5UnitRCA gfx_rca;
+
+extern const unsigned char eyecenter[];
+
+extern const unsigned char eyeright1[];
+extern const unsigned char eyeright2[];
+
+extern const unsigned char eyeleft1[];
+extern const unsigned char eyeleft2[];
+
+extern const unsigned char eyeblink1[];
+extern const unsigned char eyeblink2[];
+extern const unsigned char eyeblink3[];
+extern const unsigned char eyeblink4[];
+extern const unsigned char eyeblink5[];
+
+extern const unsigned char eyeupperleft2[];
+extern const unsigned char eyeupperright2[];
+extern const unsigned char eyelowerleft2[];
+extern const unsigned char eyelowerright2[];
+
+enum State { LEFT, CENTER, RIGHT } state;
+
+int micValue = 0;
+double capture[HORIZONTAL]; 
+double oldCapture[HORIZONTAL];
+
+// Floats for ADC voltage & Input voltage
+float adc_voltage = 0.0;
+float in_voltage = 0.0;
+ 
+// Floats for resistor values in divider (in ohms)
+float R1 = 30000.0;
+float R2 = 7500.0; 
+ 
+// Float for Reference Voltage
+float ref_voltage = 3.3; //5.0;
+ 
+// Integer for ADC value
+int adc_value = 0;
+byte eye_x, eye_y;
+
+
+ 
+void draweyes(void*) {
+    while (true) {
+  eye_x = 15;
+  eye_y = 15;
+
+  if (random(0, 100) % 50 > 35 && state == RIGHT) {
+    gfx_rca.pushImage(eye_x, eye_y, 88, 88,eyeupperright2);
+    gfx_rca.pushImage(eye_x+100, eye_y, 88, 88, eyeupperright2);
+    delay(250);
+  }
+
+  if (random(0, 100) % 50 > 35 && state == RIGHT) {
+    gfx_rca.pushImage(eye_x, eye_y, 88, 88,eyelowerright2);
+    gfx_rca.pushImage(eye_x+100, eye_y, 88, 88, eyelowerright2);
+    delay(250);
+  }
+
+if (random(0, 100) % 50 > 35 && state == LEFT) {
+    gfx_rca.pushImage(eye_x, eye_y, 88, 88,eyeupperleft2);
+    gfx_rca.pushImage(eye_x+100, eye_y, 88, 88, eyeupperleft2);
+    delay(250);
+  }
+
+  if (random(0, 100) % 50 > 35 && state == LEFT) {
+    gfx_rca.pushImage(eye_x, eye_y, 88, 88,eyelowerleft2);
+    gfx_rca.pushImage(eye_x+100, eye_y, 88, 88, eyelowerleft2);
+    delay(250);
+  }
+  if (random(0,100) % 50 > 35 && state != CENTER) {
+    if (state == RIGHT) {
+      gfx_rca.pushImage(eye_x, eye_y, 88,88, eyeright1);
+      gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeright1);
+      delay(200);
+    }
+    if (state == LEFT) {
+      gfx_rca.pushImage(eye_x, eye_y, 88,88,eyeleft1);
+      gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeleft1);
+      delay(200);
+    }
+  state = CENTER;
+  gfx_rca.pushImage(eye_x, eye_y, 88,88,eyecenter);  
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyecenter); 
+  delay(500);
+  }
+  
+  if (random(0,100) % 50 > 48 && state == CENTER) {
+    state = RIGHT;
+  gfx_rca.pushImage(eye_x, eye_y, 88,88, eyeright1);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeright1);
+  delay(200);
+  gfx_rca.pushImage(eye_x, eye_y, 88,88, eyeright2);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeright2);
+  delay(500);
+  //gfx_rca.pushImage(eye_x, eye_y, 88,88, eyeright1);
+  //gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeright1);
+  //delay(200);
+  }
+
+  //state=CENTER;
+  //gfx_rca.pushImage(eye_x, eye_y,88,88, eyecenter);
+  //gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyecenter);
+  //delay(200);
+
+  if (random(0,100) % 50 > 48 && state == CENTER) {
+    state = LEFT;
+  
+  gfx_rca.pushImage(eye_x, eye_y, 88,88, eyeleft1);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeleft1);
+  delay(200);
+  gfx_rca.pushImage(eye_x, eye_y, 88,88, eyeleft2);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeleft2);
+  delay(500);
+  //gfx_rca.pushImage(eye_x, eye_y, 88,88,eyeleft1);
+  //gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeleft1);
+  //delay(200);
+  }
+  
+if (random(0,100) % 50 > 47 && state == CENTER) {
+  gfx_rca.pushImage(eye_x, eye_y, 88,88, eyeblink1);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeblink1);
+  delay(50);
+  gfx_rca.pushImage(eye_x, eye_y, 88,88, eyeblink2);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeblink2);
+  delay(50);
+  gfx_rca.pushImage(eye_x, eye_y, 88,88, eyeblink3);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeblink3);
+  delay(50);
+  gfx_rca.pushImage(eye_x, eye_y, 88,88,eyeblink4);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeblink4);
+  delay(50);
+  gfx_rca.pushImage(eye_x, eye_y, 88,88, eyeblink5);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeblink5);
+  delay(50);
+  gfx_rca.pushImage(eye_x, eye_y, 88,88,eyeblink4);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeblink4);
+  delay(50);
+  gfx_rca.pushImage(eye_x, eye_y, 88,88,eyeblink3);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeblink3);
+  delay(50);
+  gfx_rca.pushImage(eye_x, eye_y, 88,88,eyeblink2);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeblink2);
+  delay(50);
+  gfx_rca.pushImage(eye_x, eye_y, 88,88,eyeblink1);
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyeblink1);
+  delay(50);
+        }
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    //pinMode(M5STACKFIRE_MICROPHONE_PIN, INPUT_PULLUP);
+    analogReadResolution(10);
+    gfx_rca.init();
+    gfx_rca.setFont(&DejaVu24);
+    gfx_rca.startWrite();
+    //gfx_rca.setTextColor(TFT_WHITE, TFT_BLACK);
+    //gfx_rca.drawCenterString("M5 RCA Module :)", 120, 50);
+     xTaskCreatePinnedToCore(draweyes, "draweyes", 8192, NULL, 1, NULL, 1);
+     state = CENTER;
+     eye_x = 15;
+eye_y = 15;
+  gfx_rca.pushImage(eye_x, eye_y, 88,88,eyecenter);  
+  gfx_rca.pushImage(eye_x+100, eye_y, 88,88,eyecenter); 
+}
+
+
+void loop() {
+  
+  
+for (int i=0; i<HORIZONTAL; i++) {
+       int /*float*/ readMicValue = analogRead(M5STACKFIRE_MICROPHONE_PIN);
+	   adc_voltage  = (readMicValue * ref_voltage) / 1024.0;
+       in_voltage = (adc_voltage*(R1+R2)/R2); // * 2.5;
+       Serial.print(readMicValue); //in_voltage);
+       Serial.print(','); Serial.print(in_voltage); Serial.print(',');
+       
+       int temp = map(readMicValue, 0, 200, 0, 10); //works, display is inverted, works with (oldCapture[i+1]/2)+75 etc
+       randomSeed(random(0, readMicValue));
+       //add some random noise
+       if (temp == 0) {capture[i] = random(0,2); } else {
+       capture[i] = temp; }
+       Serial.println(temp); 
+   }
+        
+for (int i=1; i<HORIZONTAL-1; i++) {
+
+  //this works but fabs is kind of pointless here
+  //    gfx_rca.drawLine(i, (fabs((oldCapture[i])/1.65))+75, i, -(fabs((oldCapture[i+1])/1.65))+75, BLACK);
+  //    gfx_rca.drawLine(i, (fabs((capture[i])/1.65))+75, i, -(fabs((capture[i+1])/1.65))+75, GREEN);
+
+  //this also works, and I kind of like it
+  //gfx_rca.drawLine(i, (((oldCapture[i])/1.65))+75, i, -(((oldCapture[i+1])/1.65))+75, BLACK);
+  //gfx_rca.drawLine(i, (((capture[i])/1.65))+75, i, -(((capture[i+1])/1.65))+75, GREEN);
+
+  
+  //gfx_rca.drawLine(i, -(((oldCapture[i])))+125, i, -(((oldCapture[i+1])))+125, BLACK);
+  //gfx_rca.drawLine(i, -(((capture[i])))+125, i, -(((capture[i+1])))+125, GREEN);
+  //gfx_rca.drawLine(i, (((oldCapture[i])))+115, i, (((oldCapture[i+1])))+115, BLACK);
+  //gfx_rca.drawLine(i, (((capture[i])))+75, i, (((capture[i+1])))+75, GREEN);
+
+  gfx_rca.drawLine(i, oldCapture[i]+105, i, oldCapture[i+1]+105, BLACK);
+  gfx_rca.drawLine(i, capture[i]+105, i, capture[i+1]+105, GREEN);
+      oldCapture[i] = capture[i];
+  }
+ 
+}
+
+//voltage sensor info
+//https://lastminuteengineers.com/voltage-sensor-arduino-tutorial/
+//https://www.electronicshub.org/interfacing-voltage-sensor-with-arduino/
